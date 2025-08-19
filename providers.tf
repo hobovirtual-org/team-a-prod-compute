a5e75ed13a96e02fb8e910de200769e0db68c443
hobovirtual <christian.renaud@hashicorp.com>
2025-08-19 15:34:43 -0400

    testing with role

--- providers.tf
+++ providers.tf
@@ -8,8 +8,11 @@ terraform {
 }
 
 provider "aws" {
-  access_key  = var.AWS_ACCESS_KEY_ID
-  secret_key  = var.AWS_SECRET_ACCESS_KEY
-  token       = var.AWS_SESSION_TOKEN
+  # access_key  = var.AWS_ACCESS_KEY_ID
+  # secret_key  = var.AWS_SECRET_ACCESS_KEY
+  # token       = var.AWS_SESSION_TOKEN  
   region      = var.REGION
+  assume_role {
+    role_arn = "arn:aws:iam::602343948585:role/aws_christian.renaud_test-admin"
+  }
 }
