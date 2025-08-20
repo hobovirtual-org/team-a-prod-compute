4bf98bc342127137078e733e94edffcda846797a
hobovirtual <christian.renaud@hashicorp.com>
2025-08-20 12:30:51 -0400

    test

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "6.9.0"
     }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
   }
 }
 
@@ -12,4 +16,16 @@ provider "aws" {
   secret_key  = var.AWS_SECRET_ACCESS_KEY
   token       = var.AWS_SESSION_TOKEN  
   region      = var.REGION
+}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+
+  role_arn = "arn:aws:iam::602343948585:role/doormat"
+}
+
+provider "aws" {
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
 }
